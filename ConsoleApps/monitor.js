9993de90af063a75c518522d739dbf712b7487dc
Daniel Kershaw <danjamker@gmail.com>
2014-10-25 19:09:50 +0100

    Deploy

--- ConsoleApps/monitor.js
+++ ConsoleApps/monitor.js
@@ -1,18 +1,36 @@
-var mongoose = require ("mongoose"); // The reason for this demo.
+var mongoose = require('mongoose')
 var database = require('../config/server');
-var Users = require('../models/User')
-
-mongoose.connect(database.url);
+var User = require('../models/User');
 var LastfmAPI = require('lastfmapi');
-
 
 var lfm = new LastfmAPI({
     'api_key' : 'c73cf13a70773cbeec50be565e3036cc',
     'secret' : '732324ecd90ac58fa0c86016e5ce8539'
 });
 
-while(true){
-    Users.find({},function(err, users){
+mongoose.connection.once('connected', function() {
+    console.log("Connected to database")
+    console.log()
+    User.find({"LastFMID":{"$exists":true}},function(err, users){
+            if (err) return console.error(err);
+            for (var i=0;i<users.length;i++){
+                var t = findrecent(users[i]["LastFMID"])
 
+            }
     });
+});
+
+var findrecent = function(username){
+    lfm.user.getRecentTracks({"user":username}, function(error, sess){
+        if(error){console.log(error)}
+        parsetacks(sess)
+    })
 }
+
+var parsetacks = function(t){
+    for(var k = 0; k < t["track"].length; k++){
+        console.log(t["track"][k]["name"])
+    }
+}
+var t = mongoose.connect(database.url);
+
