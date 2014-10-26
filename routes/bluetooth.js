16a2d9b7ab1c3f8ab9227472c8f6c5b4c97fb389
Daniel Kershaw <danjamker@gmail.com>
2014-10-26 10:35:48 +0000

    Deploy

--- routes/bluetooth.js
+++ routes/bluetooth.js
@@ -147,9 +147,9 @@ router.post('/add', function(req, res) {
                             console.log('Something went wrong!', err);
                         });
 
-                    User.findOne({SSID:req.param("SSID")},null, {sort: {listendAt: 1 }},function(err, data){
+                    User.findOne({SSID:req.param("SSID")},function(err, data){
                         PlayList.findOne({username:obj.username,playlistName:"Manchester"},function(errre,hghg) {
-                            Track.findOne({username:obj.username}, function(errr, daata){
+                            Track.findOne({username:obj.username},null, {sort: {listendAt: 1 }}, function(errr, daata){
                             console.log(daata)
                                 var t = "love"
                                 if(daata != null){
