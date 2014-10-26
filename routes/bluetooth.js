f4e5d4f9431a2e89d96de1084b3964b9804556a1
Daniel Kershaw <danjamker@gmail.com>
2014-10-26 01:49:17 +0000

    Deploy

--- routes/bluetooth.js
+++ routes/bluetooth.js
@@ -23,50 +23,6 @@ router.post('/add', function(req, res) {
 
                 console.log("here")
                 console.log(obj.SpotifyToken)
-                spotifyApi.clientCredentialsGrant()
-                    .then(function(data) {
-                        console.log('The access token expires in ' + data['expires_in']);
-                        console.log('The access token is ' + data['access_token']);
-
-                        // Save the access token so that it's used in future calls
-                        spotifyApi.setAccessToken(data['access_token']);
-
-
-
-
-                        spotifyApi.authorizationCodeGrant(obj.SpotifyToken)
-                            .then(function(data) {
-                                console.log('Retrieved access token', data['access_token']);
-
-                                // Set the access token
-                                spotifyApi.setAccessToken(data['access_token']);
-                                console.log('The access token expires in ' + data['expires_in']);
-                                console.log('The access token is ' + data['access_token']);
-                                ///THIS TOKEN NEEDS TO BE STORED!!!!!
-
-                                // Use the access token to retrieve information about the user connected to it
-                                return spotifyApi.getMe();
-                            })
-                            .then(function(data) {
-                                // "Retrieved data for Faruk Sahin"
-                                console.log('Retrieved data for ' + data['display_name']);
-
-                                // "Email is farukemresahin@gmail.com"
-                                console.log('Email is ' + data.email);
-
-                                // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
-                                console.log('Image URL is ' + data.images[0].url);
-
-                                // "This user has a premium account"
-                                console.log('This user has a ' + data.product + ' account');
-                            })
-                            .catch(function(err) {
-                                console.log('Something went wrong', err);
-                            });
-
-                    }, function(err) {
-                        console.log('Something went wrong when retrieving an access token', err);
-                    });;
 
                 var b = new BlueTooth({
                     username: obj.username,
@@ -91,6 +47,28 @@ router.post('/add', function(req, res) {
                         {currentCity:rus.results[2]},
                         function (err, obj) {
                         })
+
+                    // Set the access token
+                    spotifyApi.setAccessToken(obj.access_token_spotify);
+                    spotifyApi.getUserPlaylists(obj.spotify_id)
+                        .then(function(data) {
+                            var t = false
+                            for(var j = 0; j < data.items.length; j++){
+                                if(data.items[j].name == rus.results[2]){
+                                    t = true
+                                }
+                                if(t == false){
+                                    spotifyApi.createPlaylist(obj.spotify_id, rus.results[2], { 'public' : true })
+                                        .then(function(data) {
+                                            console.log('Created playlist!');
+                                        }, function(err) {
+                                            console.log('Something went wrong!', err);
+                                        });
+                                }
+                            }
+                        },function(err) {
+                            console.log('Something went wrong!', err);
+                        });
 
 
 
