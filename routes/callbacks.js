ef0d744c415cb57bd718befc1609ba89795c735f
Daniel Kershaw <danjamker@gmail.com>
2014-10-26 01:04:30 +0000

    Deploy

--- routes/callbacks.js
+++ routes/callbacks.js
@@ -26,53 +26,91 @@ router.get('/spotify', function(req, res) {
     {SpotifyToken:req.param("code")},
         function(err,obj) {
             if( !err ) {
-
-
-
-
-
-                                spotifyApi.authorizationCodeGrant(req.param("code"))
-                                    .then(function(data) {
-                                        console.log('Retrieved access token', data['access_token']);
-                                        User.update({username:req.param("state")},{access_token_spotify:data['access_token']}, function(err,obj){console.log(obj)})
-
-                                        // Set the access token
-                                        spotifyApi.setAccessToken(data['access_token']);
-                                        console.log('The access token expires in ' + data['expires_in']);
-                                        console.log('The access token is ' + data['access_token']);
-                                        ///THIS TOKEN NEEDS TO BE STORED!!!!!
-
-                                        // Use the access token to retrieve information about the user connected to it
-                                        return spotifyApi.getMe();
-                                    })
-                                    .then(function(data) {
-                                        // "Retrieved data for Faruk Sahin"
-                                        console.log('Retrieved data for ' + data['display_name']);
-
-                                        // "Email is farukemresahin@gmail.com"
-                                        console.log('Email is ' + data.email);
-
-                                        // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
-                                        console.log('Image URL is ' + data.images[0].url);
-
-                                        // "This user has a premium account"
-                                        console.log('This user has a ' + data.product + ' account');
-                                    })
-                                    .catch(function(err) {
-                                        console.log('Something went wrong', err);
-                                    });
-
-
-
                 console.log( 'created' );
                 res.writeHeader(200, {"Content-Type": "text/html"});
                 res.write("<button href='javascript:auth.close()'>Done</button>" );
                 res.end();
-                return;
             } else {
                 console.log( err );
                 return res.send(err);
             }
+        })
+
+    User.findOne(
+        {username: req.param("state")},
+        function (err, obj) {
+
+            console.log("here")
+            console.log(obj.SpotifyToken)
+            spotifyApi.clientCredentialsGrant()
+                .then(function(data) {
+                    console.log('The access token expires in ' + data['expires_in']);
+                    console.log('The access token is ' + data['access_token']);
+
+                    // Save the access token so that it's used in future calls
+                    spotifyApi.setAccessToken(data['access_token']);
+                    spotifyApi.authorizationCodeGrant(obj.SpotifyToken)
+                        .then(function(data) {
+                            console.log('Retrieved access token', data['access_token']);
+
+                            // Set the access token
+                            spotifyApi.setAccessToken(data['access_token']);
+                            console.log('The access token expires in ' + data['expires_in']);
+                            console.log('The access token is ' + data['access_token']);
+                            ///THIS TOKEN NEEDS TO BE STORED!!!!!
+
+                            // Use the access token to retrieve information about the user connected to it
+                            return spotifyApi.getMe();
+                        })
+                        .then(function(data) {
+                            // "Retrieved data for Faruk Sahin"
+                            console.log('Retrieved data for ' + data['display_name']);
+
+                            // "Email is farukemresahin@gmail.com"
+                            console.log('Email is ' + data.email);
+
+                            // "Image URL is http://media.giphy.com/media/Aab07O5PYOmQ/giphy.gif"
+                            console.log('Image URL is ' + data.images[0].url);
+
+                            // "This user has a premium account"
+                            console.log('This user has a ' + data.product + ' account');
+                        })
+                        .catch(function(err) {
+                            console.log('Something went wrong', err);
+                        });
+
+                }, function(err) {
+                    console.log('Something went wrong when retrieving an access token', err);
+                });;
+
+            var b = new BlueTooth({
+                username: obj.username,
+                SSID: req.param("SSID"),
+                Longitude: req.param("Longitude"),
+                Lattitude: req.param("Latitude"),
+                mac: req.param("mac")
+            });
+
+            b.save(function (err) {
+                if (!err) {
+                    console.log('created');
+                    return res.send(b);
+                } else {
+                    console.log(err);
+                    return res.send(err);
+                }
+            });
+            geocoder.reverseGeocode(req.param("Latitude"),req.param("Longitude"), function(err, rus) {
+                User.update({
+                        username: obj.username},
+                    {currentCity:rus.results[2]},
+                    function (err, obj) {
+                    })
+
+
+
+            });
+
         })
 });
 router.get('/lastfm', function(req, res) {
